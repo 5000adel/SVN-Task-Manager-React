b2c65a0fc028cee00447c1ee7a2951107e50076d
adel <xsrillixon@gmail.com>
2026-03-10 12:15:49 +0800

    feat: added role feature for role assigned views in dashboard

--- svn-app/src/assets/pages/Dashboard.jsx
+++ svn-app/src/assets/pages/Dashboard.jsx
@@ -2,8 +2,16 @@ import SideMenu from "../components/SideMenu";
 import TaskView from "../components/TaskView";
 import Background from "../components/Background"
 import TopBar from "../components/TopBar";
-export default function Dashboard({onLogOut}) {
+import SupervisorView from "../components/SupervisorView";
+export default function Dashboard({ onLogOut , role}) {
+    let view;
+
+    // admin and supervisor to be added
+    if (role === "admin") view = <TaskView />;
+    else if (role === "worker") view = <TaskView />;
+    else if (role === "supervisor") view = <SupervisorView />;
     return (
+
         <Background>
             <div style={{
                 display: 'flex',
@@ -14,20 +22,21 @@ export default function Dashboard({onLogOut}) {
             }}>
                 <div style={{
                     display: 'flex',
-                    flexDirection:'column',
+                    flexDirection: 'column',
                     justifyContent: 'center',
                     gap: '20px',
                     flexGrow: '0'
                 }}>
-                    <TopBar onLogOut={onLogOut}/>
+                    <TopBar onLogOut={onLogOut} />
                     <div style={{
                         display: 'flex',
                         justifyContent: 'center',
                         gap: '20px',
                         flexGrow: '0'
                     }}>
+
                         <SideMenu />
-                        <TaskView />
+                        {view}
                     </div>
                 </div>
 
