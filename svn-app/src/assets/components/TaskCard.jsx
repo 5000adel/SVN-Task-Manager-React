5a61e85aee085ace9151e6a40376001c01236659
adel <xsrillixon@gmail.com>
2026-03-10 13:55:14 +0800

    feat: new TaskCard.jsx & TaskCard.css to be used for supervisor dashboard view

---
+++ svn-app/src/assets/components/TaskCard.jsx
@@ -0,0 +1,13 @@
+import './styles/TaskCard.css'
+export default function TaskCard() {
+    return (
+
+        <div className="task-card-object-container" style={{ flexDirection: 'column' }}>
+            <div style={{ fontWeight: '700', fontSize: '14px', fontFamily: '"Syne", sans-serif'}}>[Task Placeholder]</div>
+            <div className="task-card-status-container">
+                {/* confirmed finished task */}
+                <div><span>Status:</span> [placeholder] </div>
+            </div>
+        </div>
+    )
+}
